e600dc0e58ccb08edf4238504355e5e88e170c22
r3volved <32720839+r3volved@users.noreply.github.com>
2023-09-12 15:15:36 -0400

    Add files via upload

--- build.js
+++ build.js
@@ -2,5 +2,7 @@ if( !process.argv[2]?.length ) {
     console.log(`Build a full cve aggregate with: $ node build /path/to/cves.json`)
     process.exit(-1)
 }
-const CVEAggregate = require('.')
+const path = require('path')
+const CVEAggregate = require(path.join(__dirname, 'index.js'))
+
 new CVEAggregate(process.argv[2], true).build()
