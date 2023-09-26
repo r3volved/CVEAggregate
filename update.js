4c47ea549eb33615246b2a5c29e1564c4c98bbd2
r3volved <32720839+r3volved@users.noreply.github.com>
2023-09-26 14:33:39 -0400

    Add files via upload

---
+++ update.js
@@ -0,0 +1,18 @@
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const fileJson = process.argv[2]
+if( !fileJson?.length ) {
+    console.error('Error: No export file specified\n')
+    console.log(`Build a full CVE aggregate with:\n$ node ${join(__dirname, 'update.js')} /path/to/cves.json`)
+    process.exit(-1)
+}
+
+const cveLibPath = join(__dirname, 'src', 'index.js')
+const { CVEAggregate } = await import(cveLibPath)
+const aggregate = new CVEAggregate(fileJson, true)
+
+await aggregate.update(true)
+await aggregate.logger.log("-".repeat(30))
+await aggregate.report()
